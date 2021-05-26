5428992dc86eaf4bc020a57ddd9e9d32ece7d2ad
Frank McDermott <fmcdermott4@gmail.com>
2021-05-26 10:23:09 -0400

    Prompts working

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-var mysql      = require('mysql2');
+var mysql = require('mysql2');
 var connection = mysql.createConnection({
   host     : 'localhost',
   user     : 'root',
@@ -7,6 +7,7 @@ var connection = mysql.createConnection({
 });
 connection.connect();
 const cTable = require('console.table');
+const inquirer = require('inquirer');
 console.log("                                                                       \n                                                                       \nCCCCCCCCCCCCCMMMMMMMM               MMMMMMMM   SSSSSSSSSSSSSSS \nCCC::::::::::::CM:::::::M             M:::::::M SS:::::::::::::::S\nCC:::::::::::::::CM::::::::M           M::::::::MS:::::SSSSSS::::::S\nC:::::CCCCCCCC::::CM:::::::::M         M:::::::::MS:::::S     SSSSSSS\nC:::::C       CCCCCCM::::::::::M       M::::::::::MS:::::S            \nC:::::C              M:::::::::::M     M:::::::::::MS:::::S            \nC:::::C              M:::::::M::::M   M::::M:::::::M S::::SSSS         \nC:::::C              M::::::M M::::M M::::M M::::::M  SS::::::SSSSS    \nC:::::C              M::::::M  M::::M::::M  M::::::M    SSS::::::::SS  \nC:::::C              M::::::M   M:::::::M   M::::::M       SSSSSS::::S \nC:::::C              M::::::M    M:::::M    M::::::M            S:::::S\nC:::::C       CCCCCCM::::::M     MMMMM     M::::::M            S:::::S\nC:::::CCCCCCCC::::CM::::::M               M::::::MSSSSSSS     S:::::S\nCC:::::::::::::::CM::::::M               M::::::MS::::::SSSSSS:::::S\nCCC::::::::::::CM::::::M               M::::::MS:::::::::::::::SS \nCCCCCCCCCCCCCMMMMMMMM               MMMMMMMM SSSSSSSSSSSSSSS   \n                                                               ")
 const menuOptions = [
     {
@@ -14,9 +15,56 @@ const menuOptions = [
         name: 'doNext',
         message: 'What would you like to do?',
         choices: [
-            'Add departments, roles, employees',
-            'View departments, roles, employees',
-            'Update employee roles'
+            'Add departments',            
+            'Add roles',
+            'Add employees',
+            'View departments',
+            'View roles',
+            'View employees',
+            'Update employee roles',
+            'Exit'
         ]
     }
-]
+];
+function menuOptionsPrompt() {
+    inquirer.prompt(menuOptions).then((answer) => {
+        switch(answer.doNext){
+            case 'Add departments':
+                console.log("Add departments");
+                addDepartments();
+                break;
+            case 'Add roles':
+                console.log("Add roles");
+                addRoles();
+                break;
+            case 'Add employees':
+                console.log("Add roles");
+                addEmployees();
+                break;
+            case 'View departments':
+                console.log("View departments");
+                viewDepartments();
+                break;
+            case 'View roles':
+                console.log("View roles");
+                viewRoles();
+                break;
+            case 'View employees':
+                console.log("View employees");
+                viewEmployees();
+                break;
+            case 'Update employee roles':
+                console.log("Update employee roles");
+                updateEmployeeRoles();
+                break;
+            case 'Exit':
+                console.log("Goodbye!");
+                process.exit(0);
+                break;   
+            default:
+                console.log("Not an option");
+                break;
+        }
+    });
+};
+menuOptionsPrompt();
